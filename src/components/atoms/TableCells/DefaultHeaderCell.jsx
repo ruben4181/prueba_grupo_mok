f84b002ed92340a348319a5d30fe80f8574f0bf1
ruben4181 <ruben4181@gmail.com>
2023-06-23 12:15:11 -0500

    cursor pointer

--- src/components/atoms/TableCells/DefaultHeaderCell.jsx
+++ src/components/atoms/TableCells/DefaultHeaderCell.jsx
@@ -1,7 +1,7 @@
 export default function DefaultHeaderCell({ value, onClick }) {
   return (
     <th
-      className="text-black dark:text-white text-center pt-5 pb-2"
+      className="text-black dark:text-white text-center pt-5 pb-2 cursor-pointer"
       onClick={onClick}
     >
       {value}
